d0cb5823525b0f84aa5dff315aae8b06273958aa
sergei11919ze <108266652+sergei11919ze@users.noreply.github.com>
2024-08-21 22:22:35 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   
-  NODE_ENV: '"production"',      
+    
   API_HOST:'"http://81.94.150.58:3000/"', 
   transpileDependencies: true, 
 })
