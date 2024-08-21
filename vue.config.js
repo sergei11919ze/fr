3413b23f8ac79cbbd75ee169b4a15bc1b077827e
sergei11919ze <108266652+sergei11919ze@users.noreply.github.com>
2024-08-21 21:09:15 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+I'mconst { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true, 
   devServer: {
-    proxy: 'http://81.94.150.58:3000'
+    proxy: 'http://81.94.150.58:3000/'
   }
 })
