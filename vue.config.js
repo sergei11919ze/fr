9c5373dea482f23fef4bee6f70fcabeef804e2b5
sergei11919ze <108266652+sergei11919ze@users.noreply.github.com>
2024-08-22 10:34:21 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+var webpack = require('webpack')
 
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'API_BASE_URL': 'http://81.94.150.58:3000/',
+          'HOST': 'https://fr-4bdc.onrender.com/'
+        }
+      })
+    ]
+  },
+  // options...
+  devServer: {
+    open: process.platform === 'darwin',
+    host: process.env.HOST,
+    https: false,
+    disableHostCheck: true
+  }
+}
